0d0e4bb61e468028db0f1648befbbafc35a8d728
kurax <kurax@wowace>
2008-03-30 07:47:32 +0000

    zhCN Bug fix

--- Localization/zhCN.lua
+++ Localization/zhCN.lua
@@ -1,8 +1,8 @@
 ﻿local MAJOR_VERSION = "LibDogTag-3.0"
 local MINOR_VERSION = tonumber(("$Revision$"):match("%d+")) or 0
 
-if MINOR_VERSION > _G.DogTag_Unit_MINOR_VERSION then
-	_G.DogTag_Unit_MINOR_VERSION = MINOR_VERSION
+if MINOR_VERSION > _G.DogTag_MINOR_VERSION then
+	_G.DogTag_MINOR_VERSION = MINOR_VERSION
 end
 
 if GetLocale() == "zhCN" then
