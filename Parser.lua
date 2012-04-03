eb2ca64a559b16dd3c06e583c387c529e6c901bf
cybeloras <cybeloras@wowace>
2012-04-03 03:25:28 +0000

    Added upvalues

--- Parser.lua
+++ Parser.lua
@@ -5,6 +5,8 @@ if MINOR_VERSION > _G.DogTag_MINOR_VERSION then
 	_G.DogTag_MINOR_VERSION = MINOR_VERSION
 end
 
+local _G, string, table, math, error = _G, string, table, math, error 
+
 -- #AUTODOC_NAMESPACE DogTag
 
 DogTag_funcs[#DogTag_funcs+1] = function(DogTag)
